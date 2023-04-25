83c799cbe3a98f846002576ba241e8c593759e2e
sanketwakhare <sanketwakhare@gmail.com>
2023-04-25 18:43:27 +0530

    add code changes to support multiple input files

--- src/main/java/com/sanket/Main.java
+++ src/main/java/com/sanket/Main.java
@@ -1,26 +1,46 @@
 package com.sanket;
 
 import com.sanket.services.FileService;
+import com.sanket.services.InputFileNameFilter;
 import com.sanket.sort.merge.MergeSort;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
 
-        // read the input files
-        List<Integer> list1 = FileService.getListFromFile("files/in1.txt");
-        List<Integer> list2 = FileService.getListFromFile("files/in2.txt");
+        // read input files from given path
+        File path = new File("files");
+        String[] fileNames = path.list(InputFileNameFilter.getInstance());
 
-        MergeSort<Integer> mergeSorter = new MergeSort<>();
-        // sort 2 lists
-        List<Integer> sortedList1 = mergeSorter.sort(list1, 0, list1.size() - 1);
-        List<Integer> sortedList2 = mergeSorter.sort(list2, 0, list2.size() - 1);
-        // combine 2 sorted lists
-        List<Integer> result = mergeSorter.merge(sortedList1, sortedList2);
-        System.out.println(result);
+        if (!Objects.isNull(fileNames)) {
 
-        // write the result to file
-        FileService.writeListToFile("files/out.txt", result);
+            MergeSort<Integer> mergeSorter = new MergeSort<>();
+            List<Integer> list1 = new ArrayList<>();
+            List<Integer> list2;
+            List<Integer> result;
+
+            for (String file1 : fileNames) {
+                list2 = FileService.getListFromFile("files/" + file1);
+
+                // sort 2 lists
+                List<Integer> sortedList1 = mergeSorter.sort(list1, list1.size() > 0 ? 0 : -1, list1.size() > 0 ? list1.size() - 1 : -1);
+                List<Integer> sortedList2 = mergeSorter.sort(list2, list2.size() > 0 ? 0 : -1, list2.size() > 0 ? list2.size() - 1 : -1);
+
+                // combine 2 sorted lists
+                result = mergeSorter.merge(sortedList1, sortedList2);
+                list1 = result;
+            }
+
+            result = list1;
+            System.out.println("total elements: " + result.size());
+            System.out.println(result);
+
+            // write the result to file
+            FileService.writeListToFile("files/out.txt", result);
+        }
     }
 }
